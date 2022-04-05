01090a81345ba4d8ca03548abf446356cbfaf4db
Diamant Lelouette <git@diamant.dev>
2022-04-05 05:16:55 +0200

    ➕ `/translate` structure.

--- src/generators/commands.ts
+++ src/generators/commands.ts
@@ -8,4 +8,126 @@ const about = new SlashCommandBuilder()
   .setName('about')
   .setDescription('ℹ️ About BooblBot.');
 
-export const commands = [about];
+const translate = new SlashCommandBuilder()
+  .setName('translate')
+  .setDescription('🌏 Translating content using boobl !')
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('text')
+      .setDescription('✍️ Translate a text using BooblBot.')
+      .addStringOption((str) =>
+        str
+          .setName('content')
+          .setDescription('The text to translate.')
+          .setRequired(true)
+      )
+      .addBooleanOption((bool) =>
+        bool
+          .setName('show')
+          .setDescription('Do you want to make the translation public?')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('to')
+          .setDescription('The latest language to translate to.')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('from')
+          .setDescription('Choose the language series that will be used.')
+          .setRequired(false)
+      )
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('message')
+      .setDescription('📜 Translate a discord message using BooblBot by ID.')
+      .addStringOption((str) =>
+        str
+          .setName('id')
+          .setDescription('The ID of the message to translate.')
+          .setRequired(true)
+      )
+      .addBooleanOption((bool) =>
+        bool
+          .setName('show')
+          .setDescription('Do you want to make the translation public?')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('to')
+          .setDescription('The latest language to translate to.')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('from')
+          .setDescription('Choose the language series that will be used.')
+          .setRequired(false)
+      )
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('wikipedia')
+      .setDescription('📚 Translate a Wikipedia abstract using BooblBot.')
+      .addStringOption((str) =>
+        str
+          .setName('topic')
+          .setDescription('The topic to translate.')
+          .setRequired(true)
+      )
+      .addBooleanOption((bool) =>
+        bool
+          .setName('show')
+          .setDescription('Do you want to make the translation public?')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('to')
+          .setDescription('The latest language to translate to.')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('from')
+          .setDescription('Choose the language series that will be used.')
+          .setRequired(false)
+      )
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('joke')
+      .setDescription('🤣 Get a random joke using BooblBot and translate it.')
+      .addStringOption((str) =>
+        str
+          .setName('language')
+          .setDescription('The language of the joke.')
+          .setRequired(false)
+          .addChoice('French', 'fr')
+          .addChoice('English', 'en')
+      )
+      .addBooleanOption((bool) =>
+        bool
+          .setName('show')
+          .setDescription('Do you want to make the translation public?')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('to')
+          .setDescription('The latest language to translate to.')
+          .setRequired(false)
+      )
+      .addStringOption((str) =>
+        str
+          .setName('from')
+          .setDescription('Choose the language series that will be used.')
+          .setRequired(false)
+      )
+  );
+
+export const commands = [about, translate];
