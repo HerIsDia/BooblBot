d8eb0e191cbb15b18f6ce358712ed09f953749d9
Diamant Lelouette <git@diamant.dev>
2022-05-19 00:10:43 +0200

    ⚡ change `/translate [...]` to `/[...]` for avoiding limit.

--- src/generators/commands.ts
+++ src/generators/commands.ts
@@ -10,165 +10,157 @@ const about = new SlashCommandBuilder()
   .setName('about')
   .setDescription('ℹ️ About BooblBot.');
 
-const translate = new SlashCommandBuilder()
-  .setName('translate')
+const translateText = new SlashCommandBuilder()
+  .setName('text')
   .setDescription('🌏 Translating content using boobl !')
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('text')
-      .setDescription('✍️ Translate a text using BooblBot.')
-      .addStringOption((str) =>
-        str
-          .setName('content')
-          .setDescription('The text to translate.')
-          .setRequired(true)
-      )
-      .addStringOption((str) =>
-        str
-          .setName('to')
-          .setDescription('The latest language to translate to.')
-          .setRequired(false)
-          .addChoices(
-            languagesSelectors.map((lang) => {
-              return [lang, lang];
-            })
-          )
-      )
-      .addStringOption((str) =>
-        str
-          .setName('serie')
-          .setDescription('Choose the language serie that will be used.')
-          .setRequired(false)
-          .addChoices(
-            series().map((serie) => {
-              return [
-                `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
-                  serie.description
-                }.`,
-                serie.name,
-              ];
-            })
-          )
+  .addStringOption((str) =>
+    str
+      .setName('content')
+      .setDescription('The text to translate.')
+      .setRequired(true)
+  )
+  .addStringOption((str) =>
+    str
+      .setName('to')
+      .setDescription('The latest language to translate to.')
+      .setRequired(false)
+      .addChoices(
+        languagesSelectors.map((lang) => {
+          return [lang, lang];
+        })
       )
   )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('message')
-      .setDescription('📜 Translate a discord message using BooblBot by ID.')
-      .addStringOption((str) =>
-        str
-          .setName('id')
-          .setDescription('The ID of the message to translate.')
-          .setRequired(true)
+  .addStringOption((str) =>
+    str
+      .setName('serie')
+      .setDescription('Choose the language serie that will be used.')
+      .setRequired(false)
+      .addChoices(
+        series().map((serie) => {
+          return [
+            `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
+              serie.description
+            }.`,
+            serie.name,
+          ];
+        })
       )
-      .addStringOption((str) =>
-        str
-          .setName('to')
-          .setDescription('The latest language to translate to.')
-          .setRequired(false)
-          .addChoices(
-            languagesSelectors.map((lang) => {
-              return [lang, lang];
-            })
-          )
-      )
-      .addStringOption((str) =>
-        str
-          .setName('serie')
-          .setDescription('Choose the language series that will be used.')
-          .setRequired(false)
-          .addChoices(
-            series().map((serie) => {
-              return [
-                `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
-                  serie.description
-                }.`,
-                serie.name,
-              ];
-            })
-          )
+  );
+
+const translateMessage = new SlashCommandBuilder()
+  .setName('message')
+  .setDescription('📜 Translate a discord message using BooblBot by ID.')
+  .addStringOption((str) =>
+    str
+      .setName('id')
+      .setDescription('The ID of the message to translate.')
+      .setRequired(true)
+  )
+  .addStringOption((str) =>
+    str
+      .setName('to')
+      .setDescription('The latest language to translate to.')
+      .setRequired(false)
+      .addChoices(
+        languagesSelectors.map((lang) => {
+          return [lang, lang];
+        })
       )
   )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('wikipedia')
-      .setDescription('📚 Translate a Wikipedia abstract using BooblBot.')
-      .addStringOption((str) =>
-        str
-          .setName('topic')
-          .setDescription('The topic to translate.')
-          .setRequired(true)
+  .addStringOption((str) =>
+    str
+      .setName('serie')
+      .setDescription('Choose the language series that will be used.')
+      .setRequired(false)
+      .addChoices(
+        series().map((serie) => {
+          return [
+            `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
+              serie.description
+            }.`,
+            serie.name,
+          ];
+        })
       )
-      .addStringOption((str) =>
-        str
-          .setName('to')
-          .setDescription('The latest language to translate to.')
-          .setRequired(false)
-          .addChoices(
-            languagesSelectors.map((lang) => {
-              return [lang, lang];
-            })
-          )
-      )
-      .addStringOption((str) =>
-        str
-          .setName('serie')
-          .setDescription('Choose the language series that will be used.')
-          .setRequired(false)
-          .addChoices(
-            series().map((serie) => {
-              return [
-                `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
-                  serie.description
-                }.`,
-                serie.name,
-              ];
-            })
-          )
+  );
+
+const translateWiki = new SlashCommandBuilder()
+  .setName('wikipedia')
+  .setDescription('📚 Translate a Wikipedia abstract using BooblBot.')
+  .addStringOption((str) =>
+    str
+      .setName('topic')
+      .setDescription('The topic to translate.')
+      .setRequired(true)
+  )
+  .addStringOption((str) =>
+    str
+      .setName('to')
+      .setDescription('The latest language to translate to.')
+      .setRequired(false)
+      .addChoices(
+        languagesSelectors.map((lang) => {
+          return [lang, lang];
+        })
       )
   )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName('joke')
-      .setDescription('🤣 Get a random joke using BooblBot and translate it.')
-      .addStringOption((str) =>
-        str
-          .setName('language')
-          .setDescription('The language of the joke.')
-          .setRequired(true)
-          .addChoice('French', 'fr')
-          .addChoice('English', 'en')
-          .addChoice('Czech', 'cs')
-          .addChoice('German', 'de')
-          .addChoice('Spanish', 'es')
-          .addChoice('Portuguese', 'pt')
+  .addStringOption((str) =>
+    str
+      .setName('serie')
+      .setDescription('Choose the language series that will be used.')
+      .setRequired(false)
+      .addChoices(
+        series().map((serie) => {
+          return [
+            `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
+              serie.description
+            }.`,
+            serie.name,
+          ];
+        })
       )
-      .addStringOption((str) =>
-        str
-          .setName('to')
-          .setDescription('The latest language to translate to.')
-          .setRequired(false)
-          .addChoices(
-            languagesSelectors.map((lang) => {
-              return [lang, lang];
-            })
-          )
+  );
+
+const translateJoke = new SlashCommandBuilder()
+  .setName('joke')
+  .setDescription('🤣 Get a random joke using BooblBot and translate it.')
+  .addStringOption((str) =>
+    str
+      .setName('language')
+      .setDescription('The language of the joke.')
+      .setRequired(true)
+      .addChoice('French', 'fr')
+      .addChoice('English', 'en')
+      .addChoice('Czech', 'cs')
+      .addChoice('German', 'de')
+      .addChoice('Spanish', 'es')
+      .addChoice('Portuguese', 'pt')
+  )
+  .addStringOption((str) =>
+    str
+      .setName('to')
+      .setDescription('The latest language to translate to.')
+      .setRequired(false)
+      .addChoices(
+        languagesSelectors.map((lang) => {
+          return [lang, lang];
+        })
       )
-      .addStringOption((str) =>
-        str
-          .setName('serie')
-          .setDescription('Choose the language series that will be used.')
-          .setRequired(false)
-          .addChoices(
-            series().map((serie) => {
-              return [
-                `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
-                  serie.description
-                }.`,
-                serie.name,
-              ];
-            })
-          )
+  )
+  .addStringOption((str) =>
+    str
+      .setName('serie')
+      .setDescription('Choose the language series that will be used.')
+      .setRequired(false)
+      .addChoices(
+        series().map((serie) => {
+          return [
+            `${serie.emoji ? `${serie.emoji} ` : ''}${serie.name}: ${
+              serie.description
+            }.`,
+            serie.name,
+          ];
+        })
       )
   );
 
@@ -204,4 +196,11 @@ const settings = new SlashCommandBuilder()
       )
   );
 
-export const commands = [about, translate, settings];
+export const commands = [
+  about,
+  translateText,
+  translateMessage,
+  translateJoke,
+  translateWiki,
+  settings,
+];
