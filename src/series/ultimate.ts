c17366eb61df18897e3687b5f0c3c6c46b9a348e
Diamant Lelouette <git@diamant.dev>
2022-05-18 22:32:42 +0200

    ➕ Weekly serie !

--- src/series/ultimate.ts
+++ src/series/ultimate.ts
@@ -1,18 +1,16 @@
 import { languages } from '../languages';
 import { Language } from '../types';
 
-const generate = () => {
-  let languagesGenerated: Language[] = [];
-  for (let index = 0; index < languages.length; index++) {
-    languagesGenerated.push(languages[index]);
-  }
-  return languagesGenerated;
-};
-
 module.exports = {
   name: 'The ultimate',
   description:
     'Translate a text with a serie of ALL of the available languages',
   emoji: '🚀',
-  languages: generate(),
+  languages: () => {
+    let languagesGenerated: Language[] = [];
+    for (let index = 0; index < languages.length; index++) {
+      languagesGenerated.push(languages[index]);
+    }
+    return languagesGenerated;
+  },
 };
